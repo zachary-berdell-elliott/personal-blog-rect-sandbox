36616d5b1f71c330abe60ca68c796c09ad88e17d
Zachary Elliott <zacharyberdell@gmail.com>
2023-03-04 19:44:39 -0700

    Starting the displayed post component.

---
+++ src/components/DisplayedPost.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+
+function DisplayedPost() {
+    const postData = {
+
+    };
+
+    return(
+        <article>
+
+        </article>
+    );
+}
+
+export default DisplayedPost;
