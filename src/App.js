d8f0a6be5f98cd2b059159ac6c6d7b0b823d335f
Zachary Elliott <zacharyberdell@gmail.com>
2023-03-12 17:59:44 -0600

    Started on DisplayedPost.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddCommentBar from './components/AddCommentBar';
+import DisplayedPost from './components/DisplayedPost';
 
 function App() {
   return (
     <div className="App">
-      <AddCommentBar />
+      {/*<AddCommentBar />*/}
+      <DisplayedPost />
     </div>
   );
 }
--- src/components/DisplayedPost.js
+++ src/components/DisplayedPost.js
@@ -4,12 +4,29 @@ import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 function DisplayedPost() {
     const postData = {
-
+        images: [{
+            url: "https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png",
+            altText: "An image of the JavaScript logo."
+        }],
+        title: "Coding in JS",
+        likeCount: 200,
+        dislikeCount: 50,
+        text: "Coding in JS is a good skill to know. This blog will teach you some of those basics. It is important to understand..."
     };
+    const arrayParsed = postData.text.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+    const parsedText = arrayParsed[0] + " " + arrayParsed[1];
 
     return(
         <article>
-
+            <h3>{postData.title}</h3>
+            {postData.images[0] ? (
+                <img src={postData.images[0].url} className="displayPostImage" 
+                    alt={postData.images[0].altText} />
+            ) : (
+                <img src="/logo192.png" className="displayPostImage" 
+                    aria-hidden="true" alt=""/>
+            )}
+            <p>{parsedText}</p>
         </article>
     );
 }
