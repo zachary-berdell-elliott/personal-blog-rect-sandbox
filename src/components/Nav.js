fd8d02b7ee55c7f7dc7a22b0ab57a3f485447256
Zachary Elliott <zacharyberdell@gmail.com>
2023-07-15 16:48:24 -0600

    Added links to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,42 @@
 import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
+    const categories = [
+        {
+            name: "React"
+        },
+        {
+            name: "Deployments"
+        },
+        {
+            name: "Java"
+        },
+        {
+            name: "JavaScript"
+        }
+    ];
     return(
-        <nav></nav>
+        <nav>
+            <ul>
+                <li>
+                    <button>Categories <FontAwesomeIcon icon={faAngleDown} /></button>
+                    <ul>
+                        {categories.map(category => (
+                            <li key={category.name}>
+                                <a href={`/categories/${category.name}`}>{category.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </li>
+               <li><a href="/login">Login</a></li>
+               <li><a href="/signup">Sign Up</a></li>
+               <li><a href="/about">About</a></li>
+               <li><a href="/moddashboard">Moderator Dashboard</a></li>
+               <li><a href="makepost">Create Post</a></li>
+            </ul>
+        </nav>
     )
 }
 
